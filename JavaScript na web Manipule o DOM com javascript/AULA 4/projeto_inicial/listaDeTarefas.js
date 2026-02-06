19503beaf4e9bec44d96db8a1eb4a43268b76e96
Maghaiver <magaiverg2@gmail.com>
2026-02-06 15:04:51 -0300

    aprendendo a criar botoes via DOM

---
+++ JavaScript na web Manipule o DOM com javascript/AULA 4/projeto_inicial/listaDeTarefas.js
@@ -0,0 +1,47 @@
+( () =>{
+  const criarTarefa = (evento) => {
+  evento.preventDefault();
+
+  const lista = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const valor = input.value;
+  console.log(valor);
+
+  const tarefa = document.createElement("li");
+  tarefa.classList.add("task");
+
+  const conteudo = `<p class="content">${valor}</p>`;
+
+  tarefa.innerHTML = conteudo;
+
+  tarefa.appendChild(BotaoConcluir());
+
+  lista.appendChild(tarefa);
+
+  input.value = " ";
+};
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+novaTarefa.addEventListener("click", criarTarefa);
+
+const BotaoConcluir = (evento) => {
+  const botaoConclui = document.createElement("button");
+
+  botaoConclui.classList.add("check-button")
+  botaoConclui.innerText = "concluir"
+
+  botaoConclui.addEventListener("click", concluirTarefa);
+
+  return botaoConclui;
+};
+
+const concluirTarefa = (evento) => {
+  const botaoConclui = evento.target
+
+  const tarefaCompleta = botaoConclui.parentElement
+
+  tarefaCompleta.classList.toggle("done")
+}
+}
+)()
